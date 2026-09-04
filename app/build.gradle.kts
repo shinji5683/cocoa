3a7f81e2d68492ba18563e9b97fc6e69c357b21d
Shinji <shinjisakiyama@gmail.com>
2026-09-05 03:26:04 +0900

    fix(build): remove deprecated kotlinOptions block for AGP 9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,10 +54,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
