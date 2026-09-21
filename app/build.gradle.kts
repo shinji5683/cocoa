a7194f8be4ad81efb5601e262c27598b26d59893
Shinji <shinjisakiyama@gmail.com>
2026-09-22 01:11:43 +0900

    chore(release): bump version to 2.3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.shinji.serena"
         minSdk = 26
         targetSdk = 37
-        versionCode = 52
-        versionName = "2.3.7"
+        versionCode = 53
+        versionName = "2.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
